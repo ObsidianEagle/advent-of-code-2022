37bdeb229d5a16fae8e5e2ac8cf47f2ac7f5fead
Alex King <alex.king@and.digital>
2022-12-01 12:43:32 +0000

    Day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -14,6 +14,19 @@ pub fn part1() -> u32 {
     largest_block_sum
 }
 
+pub fn part2() -> u32 {
+    let blocks = read_file_to_blocks_of_string_lines(Path::new("./data/day1.txt"));
+
+    let mut block_sums = blocks
+        .iter()
+        .map(|b| b.iter().map(|l| l.parse::<u32>().unwrap()).sum())
+        .collect::<Vec<u32>>();
+    block_sums.sort_by(|a, b| b.partial_cmp(a).unwrap());
+
+    block_sums[0] + block_sums[1] + block_sums[2]
+}
+
 pub fn main() {
     println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
 }
