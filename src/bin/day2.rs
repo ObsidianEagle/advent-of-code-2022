bbf8ac6b80fc7380b2f3607a66533e1cb6e91980
Alex King <alex.king@and.digital>
2022-12-02 10:35:00 +0000

    Day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -31,7 +31,54 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    let lines = read_file_to_string_lines(Path::new("./data/day2.txt"));
+
+    let mut total_score: i32 = 0;
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect();
+        let their_move = chars[0];
+        let result = chars[2];
+
+        let your_move: char = match their_move {
+            'A' => match result {
+                'X' => 'C',
+                'Y' => 'A',
+                'Z' => 'B',
+                _ => panic!("Encountered unexpected move")
+            },
+            'B' => match result {
+                'X' => 'A',
+                'Y' => 'B',
+                'Z' => 'C',
+                _ => panic!("Encountered unexpected move")
+            },
+            'C' => match result {
+                'X' => 'B',
+                'Y' => 'C',
+                'Z' => 'A',
+                _ => panic!("Encountered unexpected move")
+            },
+            _ => panic!("Encountered unexpected move")
+        };
+
+        let your_move_score: i32 = match your_move {
+            'A' => 1,
+            'B' => 2,
+            'C' => 3,
+            _ => panic!("Encountered unexpected move")
+        };
+
+        let round_score: i32 = match result {
+            'X' => 0,
+            'Y' => 3,
+            'Z' => 6,
+            _ => panic!("Encountered unexpected move")
+        };
+
+        total_score += your_move_score + round_score
+    }
+
+    total_score.unsigned_abs()
 }
 
 pub fn main() {
