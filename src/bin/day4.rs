3bf08906f8700547f1bc23a2177dc0af63d1fbad
Alex King <alex.king@and.digital>
2022-12-04 19:52:31 +0000

    Day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -28,7 +28,23 @@ pub fn part1() -> u32 {
 pub fn part2() -> u32 {
     let lines = read_file_to_string_lines(Path::new("./data/day4.txt"));
 
-    0
+    lines
+        .iter()
+        .filter(|line| {
+            let split: Vec<Vec<u32>> = line
+                .split(",")
+                .map(|range| {
+                    range
+                        .split("-")
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .collect()
+                })
+                .collect();
+
+            !((split[0][1] < split[1][0]) || split[0][0] > split[1][1])
+        })
+        .collect::<Vec<&String>>()
+        .len() as u32
 }
 
 pub fn main() {
