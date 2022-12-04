ea2e9563d895e91b150e682e50b990ed88a28547
Alex King <alex.king@and.digital>
2022-12-04 19:31:35 +0000

    Day 4 part 1

---
+++ src/bin/day4.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+
+use aoc_lib::file_utils::read_file_to_string_lines;
+
+pub fn part1() -> u32 {
+    let lines = read_file_to_string_lines(Path::new("./data/day4.txt"));
+
+    lines
+        .iter()
+        .filter(|line| {
+            let split: Vec<Vec<u32>> = line
+                .split(",")
+                .map(|range| {
+                    range
+                        .split("-")
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .collect()
+                })
+                .collect();
+
+            (split[0][0] <= split[1][0] && split[0][1] >= split[1][1])
+                || (split[1][0] <= split[0][0] && split[1][1] >= split[0][1])
+        })
+        .collect::<Vec<&String>>()
+        .len() as u32
+}
+
+pub fn part2() -> u32 {
+    let lines = read_file_to_string_lines(Path::new("./data/day4.txt"));
+
+    0
+}
+
+pub fn main() {
+    println!("Part 1 result: {}", part1());
+    println!("Part 2 result: {}", part2());
+}
